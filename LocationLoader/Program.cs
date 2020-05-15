b40bc05e27b0015f33f88b6653892bf63f580988
Tassadar2499 <34176883+Tassadar2499@users.noreply.github.com>
2020-05-15 20:31:42 +0500

    location loader utilite added

---
+++ LocationLoader/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LocationLoader
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WriteGameLocations();
+		}
+
+		private static void WriteGameLocations()
+		{
+			foreach (var directory in Directory.GetDirectories("Assets/"))
+			{
+				var files = new DirectoryInfo(directory).GetFiles();
+				string getContentbyExtension(string extension) => GetContentByExtension(files, extension);
+
+				var gameLocation = new GameLocation
+				(
+					getContentbyExtension(".css"),
+					getContentbyExtension(".html"),
+					getContentbyExtension(".txt").Split(Environment.NewLine).ToList()
+				);
+
+				var jsonText = JsonConvert.SerializeObject(gameLocation);
+				var fileName = directory[directory.LastIndexOf('/')..];
+
+				if (!Directory.Exists("out/"))
+					Directory.CreateDirectory("out");
+
+				File.WriteAllText($"out/{fileName}.json", jsonText);
+			}
+		}
+
+		private static string GetContentByExtension(FileInfo[] files, string extension)
+		{
+			var file = files.FirstOrDefault(f => f.Extension == extension)
+				?? throw new Exception($"Не найден файл по расширению - {extension}");
+
+			return File.ReadAllText(file.FullName);
+		}
+	}
+}
